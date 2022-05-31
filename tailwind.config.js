f075e75478aacae119ba4b3ad7213d4e344eb6dd
Keith Riordan <keith.riordan@gmail.com>
2022-05-30 22:01:08 -0400

    Add homepage copy and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,28 @@
-const plugin = require('tailwindcss/plugin');
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
   content: [
-    './frontend/**/*.html',
-    './frontend/**/*.js',
-    './frontend/**/*.css',
-    './gratefulmorningfarm/templates/**/*.html',
+    "./frontend/**/*.html",
+    "./frontend/**/*.js",
+    "./frontend/**/*.css",
+    "./gratefulmorningfarm/templates/**/*.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        karla: ["Karla", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [
-    plugin(function({ addVariant }) {
+    plugin(function ({ addVariant }) {
       // https://www.crocodile.dev/blog/css-transitions-with-tailwind-and-htmx
-      addVariant('htmx-settling', ['&.htmx-settling', '.htmx-settling &']);
-      addVariant('htmx-request',  ['&.htmx-request',  '.htmx-request &']);
-      addVariant('htmx-swapping', ['&.htmx-swapping', '.htmx-swapping &']);
-      addVariant('htmx-added',    ['&.htmx-added',    '.htmx-added &']);
+      addVariant("htmx-settling", ["&.htmx-settling", ".htmx-settling &"]);
+      addVariant("htmx-request", ["&.htmx-request", ".htmx-request &"]);
+      addVariant("htmx-swapping", ["&.htmx-swapping", ".htmx-swapping &"]);
+      addVariant("htmx-added", ["&.htmx-added", ".htmx-added &"]);
     }),
-    require('@tailwindcss/forms'),
+    require("@tailwindcss/forms"),
   ],
-}
+};
