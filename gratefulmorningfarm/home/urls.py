c7b217b5c54c633af062b538a0cf98b061dfd5f4
Keith Riordan <keith.riordan@gmail.com>
2022-06-02 11:58:03 -0400

    Add images to gallery page

--- gratefulmorningfarm/home/urls.py
+++ gratefulmorningfarm/home/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 
-from .views import current_time, error
+from .views import current_time, error, gallery
 
 urlpatterns = [
     path(r"current-time/", current_time, name="current_time"),
     path(r"error/", error, name="error"),
     path(r"", TemplateView.as_view(template_name="index.html"), name="home"),
-    path(r"gallery/", TemplateView.as_view(template_name="gallery.html"), name="gallery"),
+    path(r"gallery/", gallery, name="gallery"),
 ]
--- gratefulmorningfarm/home/views.py
+++ gratefulmorningfarm/home/views.py
@@ -1,6 +1,10 @@
 # Create your views here.
 
+import os
+
+from django.conf import settings
 from django.contrib import messages
+from django.shortcuts import render
 from django.template.response import TemplateResponse
 
 
@@ -13,3 +17,10 @@ def current_time(request):
     """Generate the current time. Useful for testing htmx"""
     messages.info(request, "updated the current time")
     return TemplateResponse(request, "current_time.html")
+
+
+def gallery(request):
+    img_path = os.path.join(settings.STATICFILES_DIRS[0], "img/gallery")
+    img_list = os.listdir(img_path)
+    context = {"images": img_list}
+    return render(request, "gallery.html", context)
