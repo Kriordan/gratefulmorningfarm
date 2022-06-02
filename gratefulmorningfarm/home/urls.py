07dbd6ca305adf28c12e1127285ef2a0b760d09e
Keith Riordan <keith.riordan@gmail.com>
2022-06-02 18:14:25 -0400

    Add about, blog, events templates

--- gratefulmorningfarm/home/urls.py
+++ gratefulmorningfarm/home/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path(r"current-time/", current_time, name="current_time"),
     path(r"error/", error, name="error"),
     path(r"", TemplateView.as_view(template_name="index.html"), name="home"),
+    path(r"about", TemplateView.as_view(template_name="about.html"), name="about"),
     path(r"gallery/", gallery, name="gallery"),
+    path(r"events", TemplateView.as_view(template_name="events.html"), name="events"),
+    path(r"blog", TemplateView.as_view(template_name="blog.html"), name="blog"),
 ]
